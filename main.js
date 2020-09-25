dba97ffa2926ac146934bc230e19172eea817716
kchrisalis <kayecsmaranan@gmail.com>
2020-09-25 15:20:31 -0600

    styled stuff i guess

--- main.js
+++ main.js
@@ -3,3 +3,36 @@
 // Add Recipe Form
 
 // Search Page
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+var dropdown = document.getElementsByClassName("dropdownBtn");
+var i;
+
+for (let i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+// Search Recipes
+
+document.getElementById("searchRecipe").addEventListener("click", resetPage);
+
+function resetPage() {
+  document.getElementById("recipeCont").innerHTML = "";
+}
