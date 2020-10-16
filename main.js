ae12d5710a22a6a4021eaa0ea551bf351ca577ca
kchrisalis <kayecsmaranan@gmail.com>
2020-10-16 14:09:34 -0600

    minor formatting cs
    (am clown, saved smth under the name meant for the file)
    
    how to make content div transparent so I can see the background div

--- main.js
+++ main.js
@@ -24,31 +24,47 @@ function clickHandler(event) {
       // Submit Recipe Form
     } else if (event.target.id == "submitRecipe") {
       clearAll();
-      displayPage("form")
+      displayPage("form");
 
     } else if (event.target.id == "searchRecipe") {
       clearAll();
       displayPage("title-card");
+      displayPage("recipeCont");
+      document.getElementById("recipeCont").innerHTML = "";
+      for (let i = 0; i < recipeInfo.length; i++) {
+        document.getElementById("recipeCont").append(recipePrev(recipeInfo[i]));
+      }
 
     } else if (event.target.id == "returnHome") {
       clearAll();
       displayPage("title-card");
-      displayPage("recipeCont");
+      ("recipeCont");
 
     } else if (event.target.id == "toRecipe") {
       clearAll();
       displayPage("mainRecipe");
 
-    } else if (event.target.id == "about")
+    } else if (event.target.id == "about") {
       clearAll();
-      MainRecipe(recipeInfo[1]);
+      displayPage("mrDiv");
+      document.getElementById("mrDiv").innerHTML = "";
+      document.getElementById("mrDiv").append(MainRecipe(recipeInfo[0]));
+    }
   }
 }
 
 // Recipe Form Function
 document.getElementById("submitBtn").addEventListener("click", submitForm);
 
-let recipeInfo = [];
+let recipeInfo = [{
+  recipeName: "recipeName",
+  mealType: "mealType",
+  difficulty: "difficulty",
+  prepTime: "prepTime",
+  ingredients: ["i1", "i2", "i3"],
+  steps: ["s1", "s2", "s3"],
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem "
+}];
 
 function submitForm() {
   splitText();
@@ -72,61 +88,66 @@ function submitForm() {
 
 function MainRecipe(aRecipe) {
   let mrDiv = document.createElement('div');
-  let elsR = {
-    div: document.createElement('div'),
-    title: document.createElement('h1'),
-    h2: document.createElement('h2'),
-    h3: document.createElement('h3'),
-    p: document.createElement('p'),
-    button: document.createElement('button'),
-    ol: document.createElement('ol'),
-    ul: document.createElement('ul')
-  }
+
+  mrDiv.innerHTML = `
+  <h1 class="recipeName">${aRecipe.recipeName}</h1>
+  <p>${aRecipe.description}</p>
+  <div class="subRecipeMenu">
+    <div class="textCont">
+      <h3 class="subh3">Meal Type:</h3><p class="subP">${aRecipe.mealType}</p>
+    </div>
+    <div class="textCont">
+      <h3 class="subh3">Difficulty:</h3><p class="subP">${aRecipe.difficulty}</p>
+    </div>
+    <div class="textCont">
+      <h3 class="subh3">Prep Time:</h3><p class="subP">${aRecipe.prepTime}</p>
+    </div>
+  </div>
+  <h2 class="MainRecipeH2">Ingredients</h2>`
 
   // Main Recipe Container
   mrDiv.classList.add("mainRecipe");
 
-  // Recipe Name
-  elsR.title.innerHTML = `${aRecipe.recipeName}`;
-  elsR.title.classList.add("recipeName");
-  mrDiv.append(elsR.p);
-
-  // Quick Description 
-  elsR.p.innerHTML = `${aRecipe.description}`;
-  mrDiv.append(elsR.p);
-
-  // Main Recipe Sub Box
-  elsR.div.classList.add("textCont");
-  elsR.p.classList.add("subP");
-  elsR.h3.classList.add("subH3");
-
-  els.h3.innerHTML = "Meal Type:";
-  elsR.div.append(elsR.p.innerHTML = `${aRecipe.mealType}`);
-  els.h3.innerHTML = "Difficulty:";
-  elsR.div.append(elsR.p.innerHTML = `${aRecipe.difficulty}`);
-  els.h3.innerHTML = "Prep Time:";
-  elsR.div.append(elsR.p.innerHTML = `${aRecipe.prepTime}`);
-  elsR.div.append(elsR.button.innerHTML = "Favourite This Recipe");
-  mrDiv.append(elsR.div);
-
-  // Ingredients (unordered)
-  els.h2.classList.add("MainRecipeH2");
-  mrDiv.append(els.h2.innerHTML = "Ingredients");
+  // Ingredients 
+  let ulEl = document.createElement('ul');
   for (let i = 0; i < aRecipe.ingredients.length; i++) {
-    let li = document.createElement('li')
-    li.innerHTML = `${aRecipe.ingredients[i]}`;
-    els.ol.append();
+    let liEl = document.createElement('li');
+    liEl.innerHTML = aRecipe.ingredients[i];
+    ulEl.append(liEl);
   }
-  mrDiv.append(els.ol);
+  mrDiv.append(ulEl)
 
-  // Instructions (ordered)
-  mrDiv.append(els.h2.innerHTML = "Instructions");
+  // Instructions
+  let h2El = document.createElement('h2');
+  h2El.innerHTML = "Instructions";
+  h2El.classList.add("MainRecipeH2");
+  mrDiv.append(h2El);
+
+  let olEl = document.createElement('ol');
   for (let i = 0; i < aRecipe.steps.length; i++) {
-    let li = document.createElement('li')
-    li.innerHTML = `${aRecipe.steps[i]}`;
-    els.ul.append();
+    let liEl = document.createElement('li');
+    liEl.innerHTML = aRecipe.steps[i];
+    olEl.append(liEl);
   }
-  mrDiv.append(elsR.ul);
+  mrDiv.append(olEl)
 
   return mrDiv;
+}
+
+function recipePrev(aRecipe) {
+  let rcDiv = document.createElement('div');
+  rcDiv.classList.add("recipeCard");
+
+  let h2El = document.createElement('h2');
+  h2El.innerHTML = aRecipe.recipeName;
+  rcDiv.append(h2El);
+
+  let infoDiv = document.createElement('div');
+  infoDiv.innerHTML = `
+  <p><strong>Meal Type: </strong>${aRecipe.mealType}</p>
+  <p><strong>Difficulty: </strong>${aRecipe.difficulty}</p>
+  <p class="pOverflow"><strong>Description: </strong>${aRecipe.description}</p>`
+  rcDiv.append(infoDiv);
+
+  return rcDiv;
 }
